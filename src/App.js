a23bf5cb3e25b3fd589924789d63d1dfabc5de7f
Ajayi-Adeoluwa <adeoluwatemitayo@gmail.com>
2024-01-18 14:46:43 +0100

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import randomColor from "randomcolor";
+import Quote from "./Components/Quotes.js";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [quote, setQuote] = useState({});
+  const [color, setColor] = useState("");
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  const fetchQuote = async () => {
+    try {
+      const newColor = randomColor({ luminosity: "dark" });
+      const response = await axios.get("https://api.quotable.io/random");
+      const { content, author } = response.data;
+      setQuote({ text: content, author });
+      setColor(newColor);
+    } catch (error) {
+      console.error("Error fetching quote:", error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="quote-machine" style={{ backgroundColor: color }}>
+      <Quote
+        text={quote.text}
+        author={quote.author}
+        textColor={color}
+        fetchNextQuote={fetchQuote}
+      />
     </div>
   );
-}
+};
 
 export default App;
